91e2bf25bee46e1913561a8a7ebb80b08823c57c
maurice Mumbere <mauricendayango@gmail.com>
2025-05-30 19:24:21 +0100

    chore: setup initial project structure

---
+++ app.vue
@@ -0,0 +1,21 @@
+<script setup>
+useHead({
+  title:
+    "Frontend Mentor | Fylo landing page with dark theme and features grid",
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/images/favicon-32x32.png",
+    },
+  ],
+});
+</script>
+<template>
+  <div>
+    <h1 class="underline">
+      Fylo landing page with dark theme and features grid
+    </h1>
+  </div>
+</template>
