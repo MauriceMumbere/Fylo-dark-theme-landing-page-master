2e4289d58a240f43aef367af9ce43f904ae26e67
maurice Mumbere <mauricendayango@gmail.com>
2025-06-06 15:26:04 +0100

    feat(FeatureSection): create component

---
+++ components/FeatureSection.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 items-center gap-[7rem] px-7 pt-[10rem] md:py-0"
+  >
+    <div class="text-center">
+      <div class="flex justify-center">
+        <img src="../public/images/icon-access-anywhere.svg" alt="" />
+      </div>
+      <h2 class="text-2xl font-bold pt-4 pb-2">Access your files, anywhere</h2>
+      <p class="max-w-[24rem] md:m-auto">
+        The ability to use a smartphone, tablet, or computer to access your
+        account means your files follow you everywhere.
+      </p>
+    </div>
+    <!--  -->
+    <div class="text-center">
+      <div class="flex justify-center">
+        <img src="../public/images/icon-security.svg" alt="" />
+      </div>
+      <h2 class="text-2xl font-bold pt-4 pb-2">Security you can trust</h2>
+      <p class="max-w-[24rem] m-auto">
+        2-factor authentication and user-controlled encryption are just a couple
+        of the security features we allow to help secure your files.
+      </p>
+    </div>
+    <!--  -->
+    <div class="text-center">
+      <div class="flex justify-center">
+        <img src="../public/images/icon-collaboration.svg" alt="" />
+      </div>
+      <h2 class="text-2xl font-bold pt-4 pb-2">Real-time collaboration</h2>
+      <p class="max-w-[25rem] m-auto">
+        Securely share files and folders with friends, family and colleagues for
+        live collaboration. No email attachments required.
+      </p>
+    </div>
+    <!--  -->
+    <div class="text-center">
+      <div class="flex justify-center">
+        <img src="../public/images/icon-any-file.svg" alt="" />
+      </div>
+      <h2 class="text-2xl font-bold pt-4 pb-2">Store any type of file</h2>
+      <p class="max-w-[23rem] m-auto">
+        Whether you're sharing holidays photos or work documents, Fylo has you
+        covered allowing for all file types to be securely stored and shared.
+      </p>
+    </div>
+  </div>
+</template>
