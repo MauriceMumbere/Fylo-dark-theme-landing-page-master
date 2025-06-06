447a289e9fa2a007d6450eb937d8797ca4b1f56a
maurice Mumbere <mauricendayango@gmail.com>
2025-06-06 15:25:17 +0100

    feat(app): add all components

--- app.vue
+++ app.vue
@@ -10,12 +10,24 @@ useHead({
       href: "/images/favicon-32x32.png",
     },
   ],
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+    },
+  ],
 });
 </script>
 <template>
   <div>
-    <h1 class="underline">
-      Fylo landing page with dark theme and features grid
-    </h1>
+    <Header />
+    <HeroSection />
+    <div class="bg-navy-900 md:px-[7rem]">
+      <FeatureSection />
+      <ProductiveSection />
+      <TestimonialSection />
+      <NewsLetterSection />
+    </div>
+    <Footer />
   </div>
 </template>
