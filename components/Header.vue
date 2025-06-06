78cad729d4c3a7b5f916e70ff0786e3797b11cfa
maurice Mumbere <mauricendayango@gmail.com>
2025-06-06 15:26:42 +0100

    feat(Header): create component

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- Header -->
+  <header
+    class="flex justify-between items-center px-5 md:px-[4rem] py-7 md:pt-[5rem] bg-navy-850"
+  >
+    <!-- Logo -->
+    <div>
+      <img class="h-7" src="../public/images/logo.svg" alt="Logo" />
+    </div>
+    <!-- Navigation -->
+    <nav>
+      <!-- Link -->
+      <ul class="space-x-5 flex md:space-x-10">
+        <li>
+          <NuxtLink to="#">Features</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="#">Team</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="#">Sign In</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
