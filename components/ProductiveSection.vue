b5ce7b10e67e2c25d8c041438b0d8f524d9b0b59
maurice Mumbere <mauricendayango@gmail.com>
2025-06-06 15:27:50 +0100

    feat(ProductiveSection): create component

---
+++ components/ProductiveSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <section
+    class="px-[2rem] py-[10rem] flex flex-col md:flex-row items-center gap-10"
+  >
+    <div>
+      <img src="../public/images/illustration-stay-productive.png" alt="" />
+    </div>
+    <div class="space-y-5">
+      <h2 class="font-bold text-lg md:text-4xl text-center md:text-left">
+        Stay productive, wherever you are
+      </h2>
+      <p>
+        Never let location be an issue when accessing your files. Fylo has you
+        covered for all of your file storage needs.
+      </p>
+      <p>
+        Securely share files and folders with friends, family and colleagues
+        forlive collaboration. No email attachments required.
+      </p>
+
+      <a
+        href="#"
+        class="flex items-center gap-1.5 relative after:absolute after:h-[1px] after:bg-gradient-to-l after:from-Teal-200 after:to-cyan-200 after:w-40 after:top-6.5"
+      >
+        <span>See how Fylo works</span>
+        <img class="h-5" src="../public/images/icon-arrow.svg" alt="" />
+      </a>
+    </div>
+  </section>
+</template>
