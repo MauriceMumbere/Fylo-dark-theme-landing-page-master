b4e50c00a714cc5751b17fadebda96eea18f159f
maurice Mumbere <mauricendayango@gmail.com>
2025-06-06 15:27:32 +0100

    feat(NewsLetterSection): create component

---
+++ components/NewsLetterSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="py-[2rem] px-7">
+    <div
+      class="text-center bg-navy-850 w-full md:w-[65%] m-auto pt-[2.5rem] pb-[1.5rem] rounded-md relative top-40 px-4 shadow-lg"
+    >
+      <h2 class="text-xl md:text-4xl font-bold">Get early access today</h2>
+      <p class="py-5 max-w-[45rem] m-auto">
+        It only takes a minute to sign up and our free starter tier is extremely
+        generous. If you have any questions, our support team would be happy to
+        help you.
+      </p>
+      <div class="flex flex-col md:flex-row space-x-5 space-y-5">
+        <div class="w-full">
+          <input
+            class="bg-white rounded-full py-3 px-4 w-full md:w-[32rem] text-black outline-none"
+            type="text"
+          />
+        </div>
+        <div>
+          <button
+            class="bg-gradient-to-l from-Teal-200 to-cyan-200 rounded-full py-3 px-4 w-full md:w-[14rem]"
+          >
+            Get Started For Free
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
