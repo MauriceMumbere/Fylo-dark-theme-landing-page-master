69fea9e42d9cd1e8c7a15ae0c7fb96efd0574359
maurice Mumbere <mauricendayango@gmail.com>
2025-06-06 15:28:23 +0100

    feat(TestimonialSection): create component

---
+++ components/TestimonialSection.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="grid grid-cols-1 md:grid-cols-3 px-10 md:px-[4rem] gap-10 relative py-9"
+  >
+    <div class="absolute left-14 top-5 md:top-0 z-0">
+      <img class="h-5 md:h-12" src="../public/images/bg-quotes.png" alt="" />
+    </div>
+    <div class="bg-navy-800 p-5 z-30 rounded-md">
+      <p class="pb-5 text-[12px]">
+        Fylo has improved our team productivity by an order of magnitude. Since
+        making the switch our team has become a well-oiled collaboration
+        machine.
+      </p>
+      <div class="flex items-center gap-3">
+        <div>
+          <img
+            class="rounded-full h-[45px]"
+            src="../public/images/profile-1.jpg"
+            alt=""
+          />
+        </div>
+        <div class="flex flex-col">
+          <span class="font-bold text-lg"> Satish Patel </span>
+          <span class="text-[12px]"> Founder & CEO, Huddle </span>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="bg-navy-800 p-7 rounded-md">
+      <p class="pb-5">
+        Fylo has improved our team productivity by an order of magnitude. Since
+        making the switch our team has become a well-oiled collaboration
+        machine.
+      </p>
+      <div class="flex items-center gap-3">
+        <div>
+          <img
+            class="rounded-full h-[45px]"
+            src="../public/images/profile-2.jpg"
+            alt=""
+          />
+        </div>
+        <div class="flex flex-col">
+          <span class="font-bold text-lg"> Bruce McKenzie </span>
+          <span class="text-[12px]"> Founder & CEO, Huddle </span>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="bg-navy-800 p-7 rounded-md">
+      <p class="pb-5">
+        Fylo has improved our team productivity by an order of magnitude. Since
+        making the switch our team has become a well-oiled collaboration
+        machine.
+      </p>
+      <div class="flex items-center gap-3">
+        <div>
+          <img
+            class="rounded-full h-[45px]"
+            src="../public/images/profile-3.jpg"
+            alt=""
+          />
+        </div>
+        <div class="flex flex-col">
+          <span class="font-bold text-lg"> Iva Boyd </span>
+          <span class="text-[12px]">Founder & CEO, Huddle</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
