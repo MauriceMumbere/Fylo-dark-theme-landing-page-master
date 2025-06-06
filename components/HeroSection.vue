9e43b585473cead6e457b1adf73592bd976929fc
maurice Mumbere <mauricendayango@gmail.com>
2025-06-06 15:27:02 +0100

    feat(HeroSection): create component

---
+++ components/HeroSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <section
+    class="text-center md:bg-[url('public/images/bg-curvy-desktop.svg')] bg-[url('public/images/bg-curvy-mobile.svg')] bg-contain bg-no-repeat bg-bottom bg-navy-850 pt-10 md:pb-[17rem] px-4"
+  >
+    <div class="flex justify-center">
+      <img src="../public/images/illustration-intro.png" alt="" />
+    </div>
+    <h1
+      class="text-2xl md:text-4xl font-bold max-w-[40rem] md:m-auto py-7 leading-[3rem]"
+    >
+      All your files in one secure location, accessible anywhere.
+    </h1>
+    <p class="max-w-[28rem] m-auto pb-7">
+      Fylo stores all your most important files in one secure location. Access
+      them wherever you need, share and collaborate with friends family, and
+      co-workers.
+    </p>
+    <div>
+      <a
+        class="py-3 px-16 rounded-full bg-gradient-to-l from-Teal-200 to-cyan-200 font-bold"
+        href="#"
+        >Get Started</a
+      >
+    </div>
+  </section>
+</template>
