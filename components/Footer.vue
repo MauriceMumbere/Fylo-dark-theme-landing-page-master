03580b086885e23564cd3c55c430946f84b6cf17
maurice Mumbere <mauricendayango@gmail.com>
2025-06-06 15:26:26 +0100

    feat(Footer): create component

---
+++ components/Footer.vue
@@ -0,0 +1,75 @@
+<template>
+  <footer class="bg-navy-950 px-7 md:px-[10rem] pt-[13rem] pb-[5rem]">
+    <div class="">
+      <img src="../public/images/logo.svg" alt="Logo" />
+    </div>
+
+    <div class="flex flex-col md:flex-row justify-between mt-6">
+      <div class="flex gap-4 mb-5">
+        <img
+          class="h-5 mt-1.5"
+          src="../public/images/icon-location.svg"
+          alt=""
+        />
+        <p class="w-[20rem]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </p>
+      </div>
+      <div class="mb-20">
+        <ul class="leading-[2rem]">
+          <li class="flex items-center gap-4">
+            <img class="h-4" src="../public/images/icon-phone.svg" alt="" />
+            <span> +1-543-123-4567 </span>
+          </li>
+          <li class="flex items-center gap-4">
+            <img class="h-4" src="../public/images/icon-email.svg" alt="" />
+            <span>example@fylo.com</span>
+          </li>
+        </ul>
+      </div>
+      <div class="mb-15">
+        <ul class="leading-[2rem]">
+          <li>About Us</li>
+          <li>Jobs</li>
+          <li>Press</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+      <!--  -->
+      <div class="mb-20">
+        <ul class="leading-[2rem]">
+          <li>Contact Us</li>
+          <li>Terms</li>
+          <li>Privacy</li>
+        </ul>
+      </div>
+      <!--  -->
+      <div class="flex justify-center md:justify-start">
+        <ul class="flex gap-3">
+          <li>
+            <div
+              class="flex justify-center items-center border border-white h-8 w-8 rounded-full"
+            >
+              <i class="fa fa-facebook"> </i>
+            </div>
+          </li>
+          <li>
+            <div
+              class="flex justify-center items-center border border-white h-8 w-8 rounded-full"
+            >
+              <i class="fa fa-twitter"> </i>
+            </div>
+          </li>
+          <li>
+            <div
+              class="flex justify-center items-center border border-white h-8 w-8 rounded-full"
+            >
+              <i class="fa fa-instagram"> </i>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
